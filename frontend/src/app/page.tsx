9f2a6ee18d9c9608e4706af0c5bab271b2837cac
Seongchan-CHOI <chltjdcks329@naver.com>
2026-01-16 14:36:54 +0900

    <f>필요한 기술 설치

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="p-20">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello World
+      </h1>
+    </div>
+  );
+}
